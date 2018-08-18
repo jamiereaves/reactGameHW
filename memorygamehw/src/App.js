e0f30909a9c344ba8db4ff172a8558bdf9bdcf85
jamie reaves <jamiereaves@utexas.edu>
2018-08-18 04:00:10 -0500

    added endgame. small patches.

--- memorygamehw/src/App.js
+++ memorygamehw/src/App.js
@@ -15,53 +15,64 @@ class App extends Component {
     cards,
     score: 0,
     highScore: 0,
-    message: "Click on any image to earn points. Click an image twice, though & your game is over. Remember, when you play the game of beans, you win, or you die--there is no middle ground!"
+    message: "Click on any image to earn a point. If you can make it to 26 points, you keep the beans. Click an image twice, though, & your game is over...when you play the game of beans, you win, or you die. There is no middle ground!"
   };
 
   clickHandler = (x) => {
     const targetCard = this.state.cards.filter(card => card.id === x);
     console.log(targetCard[0]);
     console.log("name: " + targetCard[0].name + " || clicked: " + targetCard[0].clicked);
+    //check if the user has won
+    if (targetCard[0].clicked===false && this.state.score===25){
+      this.setState({highScore: 20});
+      this.setState({message: "Congratulations! Click any image to start again!!"});
+      this.setState({score: 0});
+      const clickedCards = this.state.cards.filter(card => card.clicked === true);
+      for (let i=0; i<clickedCards.length; i++) {
+        clickedCards[i].clicked = false;
+      };
+      
+    }
     //check to see if the card has been clicked
-      if (targetCard[0].clicked===false){
-          //if it hasn't, change the value of clicked to true, increase score, increase high score if applicable, & change message state
-          targetCard[0].clicked=true;
-          this.setState({score: this.state.score + 1}, function () {
-            if (this.state.score > this.state.highScore) {
-              this.setState({highScore: this.state.score}, function () {
-                console.log("high score: " + this.state.highScore);
-              });
-            }
-            console.log("score: " + this.state.score)});
-          this.setState({message: "You chose "+ targetCard[0].name + "."}, function () {
-            console.log(this.state.message);
-          })
-        }
-        //if it has, reset the values of all "clicked" cards, reset score to 0, & change message state
-        else{
-          console.log("LOSER LOSER LOSER");
-          const clickedCards = this.state.cards.filter(card => card.clicked === true);
-          for (let i=0; i<clickedCards.length; i++) {
-            clickedCards[i].clicked = false;
-          };
-          this.setState({score: 0}, function () {
-            console.log(this.state.score);
-            console.log(this.state.highScore);
-          });
-          this.setState({message: "You already chose " + targetCard[0].name +"...press any image to start again!"}, function () {
-            console.log(this.state.message);
-          })
-  
-        }
-        console.log("name: " + targetCard[0].name + " || clicked: " + targetCard[0].clicked);
-        for (let i = cards.length - 1; i > 0; i--) {
-          let j = Math.floor(Math.random() * (i + 1));
-          let temp = cards[i];
-          cards[i] = cards[j];
-          cards[j] = temp;
+    else if (targetCard[0].clicked===false){
+        //if it hasn't, change the value of clicked to true, increase score, increase high score if applicable, & change message state
+        targetCard[0].clicked=true;
+        this.setState({score: this.state.score + 1}, function () {
+          if (this.state.score > this.state.highScore) {
+            this.setState({highScore: this.state.score}, function () {
+              console.log("high score: " + this.state.highScore);
+            });
+          }
+          console.log("score: " + this.state.score)});
+        this.setState({message: "You chose "+ targetCard[0].name + "."}, function () {
+          console.log(this.state.message);
+        })
+      }
+      //if it has, reset the values of all "clicked" cards, reset score to 0, & change message state
+      else{
+        console.log("LOSER LOSER LOSER");
+        const clickedCards = this.state.cards.filter(card => card.clicked === true);
+        for (let i=0; i<clickedCards.length; i++) {
+          clickedCards[i].clicked = false;
         };
-        //set this.state.cards to the new cards array
-        this.setState({cards});
+        this.setState({score: 0}, function () {
+          console.log(this.state.score);
+          console.log(this.state.highScore);
+        });
+        this.setState({message: "You already chose " + targetCard[0].name +"...click any image to start again!"}, function () {
+          console.log(this.state.message);
+        })
+
+      }
+      console.log("name: " + targetCard[0].name + " || clicked: " + targetCard[0].clicked);
+      for (let i = cards.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = cards[i];
+        cards[i] = cards[j];
+        cards[j] = temp;
+      };
+      //set this.state.cards to the new cards array
+      this.setState({cards});
 
   }; 
 
